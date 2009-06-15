b92da2304fa0c528a08448da8efacd0dcfb7d9ec
Eric Nawrocki <eric.nawrocki@gmail.com>
2009-06-15 22:16:41 +0000

    git-svn-id: https://svn.janelia.org/eddylab/nawrockie/src@485 2857e7bf-25ef-0310-8806-9752169b59e1

---
+++ seeds/addrf.sh
@@ -0,0 +1,5 @@
+esl-alimanip -1 -g --gapthresh 0.8 -o archaea.rf.stk archaea.stk
+esl-alimanip -1 -g --gapthresh 0.8 -o bacteria.rf.stk bacteria.stk
+esl-alimanip -1 -g --gapthresh 0.8 -o chloroplast.rf.stk chloroplast.stk
+esl-alimanip -1 -g --gapthresh 0.8 -o eukarya.rf.stk eukarya.stk
+esl-alimanip -1 -g --gapthresh 0.8 -o metamito.rf.stk metamito.stk
