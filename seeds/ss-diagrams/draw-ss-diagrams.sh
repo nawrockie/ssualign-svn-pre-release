1b3b48dbd87871affcde8e4fd640b94586a32595
Eric Nawrocki <eric.nawrocki@gmail.com>
2009-06-17 15:19:31 +0000

    o Added seeds back.
    git-svn-id: https://svn.janelia.org/eddylab/nawrockie/src@489 2857e7bf-25ef-0310-8806-9752169b59e1

---
+++ seeds/ss-diagrams/draw-ss-diagrams.sh
@@ -0,0 +1,39 @@
+esl-ssudraw ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-info.ps
+esl-ssudraw -q -d ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-del.ps
+esl-ssudraw -q -i ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-ins.ps
+ps2pdf archaea-0p1.ps 
+ps2pdf archaea-0p1-info.ps
+ps2pdf archaea-0p1-del.ps
+ps2pdf archaea-0p1-ins.ps
+
+esl-ssudraw ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-info.ps
+esl-ssudraw -q -d ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-del.ps
+esl-ssudraw -q -i ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-ins.ps
+ps2pdf bacteria-0p1.ps 
+ps2pdf bacteria-0p1-info.ps
+ps2pdf bacteria-0p1-del.ps
+ps2pdf bacteria-0p1-ins.ps
+
+esl-ssudraw ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-info.ps
+esl-ssudraw -q -d ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-del.ps
+esl-ssudraw -q -i ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-ins.ps
+ps2pdf chloroplast-0p1.ps 
+ps2pdf chloroplast-0p1-info.ps
+ps2pdf chloroplast-0p1-del.ps
+ps2pdf chloroplast-0p1-ins.ps
+
+esl-ssudraw ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-info.ps
+esl-ssudraw -q -d ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-del.ps
+esl-ssudraw -q -i ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-ins.ps
+ps2pdf eukarya-0p1.ps 
+ps2pdf eukarya-0p1-info.ps
+ps2pdf eukarya-0p1-del.ps
+ps2pdf eukarya-0p1-ins.ps
+
+esl-ssudraw ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-info.ps
+esl-ssudraw -q -d ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-del.ps
+esl-ssudraw -q -i ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-ins.ps
+ps2pdf metamito-0p1.ps 
+ps2pdf metamito-0p1-info.ps
+ps2pdf metamito-0p1-del.ps
+ps2pdf metamito-0p1-ins.ps
