b23c6cf079cef8eece80b6502e8a4f6adb7c99b1
Eric Nawrocki <eric.nawrocki@gmail.com>
2009-07-06 15:39:15 +0000

    o All structure diagrams updated for the current esl-ssudraw. You can   redraw all of these using 'draw-ss-diagrams.sh'.
    git-svn-id: https://svn.janelia.org/eddylab/nawrockie/src@552 2857e7bf-25ef-0310-8806-9752169b59e1

--- seeds/ss-diagrams/draw-ss-diagrams.sh
+++ seeds/ss-diagrams/draw-ss-diagrams.sh
@@ -1,39 +1,51 @@
-esl-ssudraw ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-info.ps
-esl-ssudraw -q -d ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-del.ps
-esl-ssudraw -q -i ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-ins.ps
-ps2pdf archaea-0p1.ps 
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-info.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw --indi ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-rf.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --dall ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-del.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --ins ../archaea-0p1.stk archaea-0p1.ps archaea-0p1-ins.ps
+ps2pdf archaea-0p1-rf.ps 
 ps2pdf archaea-0p1-info.ps
 ps2pdf archaea-0p1-del.ps
 ps2pdf archaea-0p1-ins.ps
+cat archaea-0p1-rf.ps archaea-0p1-info.ps archaea-0p1-del.ps archaea-0p1-ins.ps > all.ps
 
-esl-ssudraw ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-info.ps
-esl-ssudraw -q -d ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-del.ps
-esl-ssudraw -q -i ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-ins.ps
-ps2pdf bacteria-0p1.ps 
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-info.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw --indi ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-rf.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --dall ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-del.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --ins ../bacteria-0p1.stk bacteria-0p1.ps bacteria-0p1-ins.ps
+ps2pdf bacteria-0p1-rf.ps 
 ps2pdf bacteria-0p1-info.ps
 ps2pdf bacteria-0p1-del.ps
 ps2pdf bacteria-0p1-ins.ps
+cat bacteria-0p1-rf.ps bacteria-0p1-info.ps bacteria-0p1-del.ps bacteria-0p1-ins.ps >> all.ps
 
-esl-ssudraw ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-info.ps
-esl-ssudraw -q -d ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-del.ps
-esl-ssudraw -q -i ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-ins.ps
-ps2pdf chloroplast-0p1.ps 
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-info.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw --indi ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-rf.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --dall ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-del.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --ins ../chloroplast-0p1.stk chloroplast-0p1.ps chloroplast-0p1-ins.ps
+ps2pdf chloroplast-0p1-rf.ps 
 ps2pdf chloroplast-0p1-info.ps
 ps2pdf chloroplast-0p1-del.ps
 ps2pdf chloroplast-0p1-ins.ps
+cat chloroplast-0p1-rf.ps chloroplast-0p1-info.ps chloroplast-0p1-del.ps chloroplast-0p1-ins.ps >> all.ps
 
-esl-ssudraw ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-info.ps
-esl-ssudraw -q -d ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-del.ps
-esl-ssudraw -q -i ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-ins.ps
-ps2pdf eukarya-0p1.ps 
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-info.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw --indi ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-rf.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --dall ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-del.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --ins ../eukarya-0p1.stk eukarya-0p1.ps eukarya-0p1-ins.ps
+ps2pdf eukarya-0p1-rf.ps 
 ps2pdf eukarya-0p1-info.ps
 ps2pdf eukarya-0p1-del.ps
 ps2pdf eukarya-0p1-ins.ps
+cat eukarya-0p1-rf.ps eukarya-0p1-info.ps eukarya-0p1-del.ps eukarya-0p1-ins.ps >> all.ps
 
-esl-ssudraw ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-info.ps
-esl-ssudraw -q -d ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-del.ps
-esl-ssudraw -q -i ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-ins.ps
-ps2pdf metamito-0p1.ps 
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-info.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw --indi ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-rf.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --dall ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-del.ps
+~/school/notebook/9_0625_ssudraw_ths_push/wd-1p0branch-infernal/easel/miniapps/esl-ssudraw -q --ins ../metamito-0p1.stk metamito-0p1.ps metamito-0p1-ins.ps
+ps2pdf metamito-0p1-rf.ps 
 ps2pdf metamito-0p1-info.ps
 ps2pdf metamito-0p1-del.ps
 ps2pdf metamito-0p1-ins.ps
+cat metamito-0p1-rf.ps metamito-0p1-info.ps metamito-0p1-del.ps metamito-0p1-ins.ps >> all.ps
+
+ps2pdf all.ps
